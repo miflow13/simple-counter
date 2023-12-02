b4e0908243b12b5c394e2073ab657b1f791f7753
Mika <pizzafan513@gmail.com>
2023-12-02 15:47:03 -0500

    Add basic interface and styling for toy clicker - refactored reset function

--- src/script.js
+++ src/script.js
@@ -2,15 +2,6 @@ window.onload = function () {
   // Declare count variable
   let clickCount = 0;
 
-
-
-
-
-
-
-
-
-
   document.getElementById("milestone").volume = 0.5;
 
   // Declare buttons from DOM
@@ -24,12 +15,11 @@ window.onload = function () {
 
   // Declare function to reset counter and win message
   const onReset = () => {
+    clickButton.style.display = "inline-block";
     clickCount = 0;
-    clickButton.style.display = "block";
     display.innerHTML = clickCount;
     message.innerHTML = "";
   };
-
 
   const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
@@ -37,13 +27,15 @@ window.onload = function () {
     try {
       const response = await fetch(url);
       if (!response.ok) {
-        throw new Error(`Failed to fetch audio file: ${response.status} ${response.statusText}`);
+        throw new Error(
+          `Failed to fetch audio file: ${response.status} ${response.statusText}`
+        );
       }
-  
+
       const audioData = await response.arrayBuffer();
       return audioContext.decodeAudioData(audioData);
     } catch (error) {
-      console.error('Error loading audio:', error.message);
+      console.error("Error loading audio:", error.message);
       throw error; // Re-throw the error to propagate it to the calling code
     }
   };
@@ -55,61 +47,47 @@ window.onload = function () {
     source.connect(audioContext.destination);
     source.start(0);
   };
-  
-
-
-
-
-
-
 
   // Function to count clicks
   const onClick = () => {
-
     if (!clickTimeout) {
       clickTimeout = setTimeout(() => {
         clickTimeout = null;
       }, 1000); // Adjust the debounce interval
     }
 
-    playClickSound('click.mp3');
+    playClickSound("click.mp3");
     clickCount++;
     display.innerHTML = clickCount;
-
-    
 
     // Check for specific click counts
     if (clickCount === 25 || clickCount === 50 || clickCount === 75) {
       document.getElementById("milestone").play();
-      message.innerHTML = `You have clicked ${clickCount} times!`;
-     
+      message.innerHTML = `You have clicked  ${clickCount} times!`;
     }
 
     if (clickCount === 100) {
       // Once user clicks 100 times, buttons will be removed from DOM, and win message will be displayed
-      display.style.fontSize = "100px";
       message.style.fontSize = "50px";
       message.innerHTML = "You win! You have clicked 100 times!";
       clickButton.style.display = "none";
     }
-
-    
-
-
 
     display.classList.add("animate");
 
     setTimeout(() => {
       display.classList.remove("animate");
     }, 500); // Adjust the time to match the animation duration
-
-    
   };
 
-  
-let clickTimeout = null;
+  let clickTimeout = null;
 
-btn.addEventListener("click", onClick);
+  // Function to handle starting over
+  const onStartOver = () => {
+    onReset(); // Call the existing reset function
+  };
+
+  btn.addEventListener("click", onClick);
 
   // Event listener for reset button and clicker
   clickButton.addEventListener("click", onClick);
