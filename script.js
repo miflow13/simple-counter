10e1aea68d0c1c719f02b2282fb9c775755a2c7c
Mika <pizzafan513@gmail.com>
2023-12-02 13:06:06 -0500

    Add message when counter reaches specific number. Add reset button to reset counter

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-
 // window.onLoad so script does not run before DOM fully loads
 window.onload = function () {
-    
-// declare count variable
+  // declare count variable
   let count = 0;
 
   // declare button from DOM
   let btn = document.getElementById("btn");
+  let resetbtn = document.getElementById("resetbtn");
 
   // declare count display from dom, where count will be displayed
   let disp = document.getElementById("display");
+  let msg = document.getElementById("msg");
 
-  btn.addEventListener("click", function () {
+  // Function to count clicks
+  const onClick = () => {
     count++;
     disp.innerHTML = count;
-    console.log("hello");
-  });
+    if (count === 10) {
+      msg.innerHTML = "Yay you clicked 10 times!";
+    }
+    if (count === 20) {
+      msg.innerHTML = "You have clicked 20 times!";
+    }
+  };
+
+  // function to reset counter and message
+  const onReset = () => {
+    count = 0;
+    disp.innerHTML = count;
+    msg.innerHTML = "";
+  };
+
+  // event lister for reset button and clicker
+  btn.addEventListener("click", onClick);
+  resetbtn.addEventListener("click", onReset);
 };
