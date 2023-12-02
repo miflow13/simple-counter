89c5f728f2e29c659427b34da45c9bd6b1b2b15a
Mika <pizzafan513@gmail.com>
2023-12-02 15:08:47 -0500

    Implement Web Audio API for Click Sound
    - Integrate Web Audio API for handling click sound
    - Address 404 error by fixing audio file path
    - Improve responsiveness during fast clicks
    - Add debouncing to prevent rapid triggering of click events

--- src/script.js
+++ src/script.js
@@ -2,6 +2,17 @@ window.onload = function () {
   // Declare count variable
   let clickCount = 0;
 
+
+
+
+
+
+
+
+
+
+  document.getElementById("milestone").volume = 0.5;
+
   // Declare buttons from DOM
   let buttonContainer = document.getElementById("button-container");
   let clickButton = document.getElementById("btn");
@@ -19,14 +30,59 @@ window.onload = function () {
     message.innerHTML = "";
   };
 
+
+  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+  const loadAudioBuffer = async (url) => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Failed to fetch audio file: ${response.status} ${response.statusText}`);
+      }
+  
+      const audioData = await response.arrayBuffer();
+      return audioContext.decodeAudioData(audioData);
+    } catch (error) {
+      console.error('Error loading audio:', error.message);
+      throw error; // Re-throw the error to propagate it to the calling code
+    }
+  };
+
+  const playClickSound = async (url) => {
+    const buffer = await loadAudioBuffer(url);
+    const source = audioContext.createBufferSource();
+    source.buffer = buffer;
+    source.connect(audioContext.destination);
+    source.start(0);
+  };
+  
+
+
+
+
+
+
+
   // Function to count clicks
   const onClick = () => {
+
+    if (!clickTimeout) {
+      clickTimeout = setTimeout(() => {
+        clickTimeout = null;
+      }, 1000); // Adjust the debounce interval
+    }
+
+    playClickSound('click.mp3');
     clickCount++;
     display.innerHTML = clickCount;
 
+    
+
     // Check for specific click counts
     if (clickCount === 25 || clickCount === 50 || clickCount === 75) {
+      document.getElementById("milestone").play();
       message.innerHTML = `You have clicked ${clickCount} times!`;
+     
     }
 
     if (clickCount === 100) {
@@ -37,12 +93,23 @@ window.onload = function () {
       clickButton.style.display = "none";
     }
 
+    
+
+
+
     display.classList.add("animate");
 
     setTimeout(() => {
       display.classList.remove("animate");
     }, 500); // Adjust the time to match the animation duration
+
+    
   };
+
+  
+let clickTimeout = null;
+
+btn.addEventListener("click", onClick);
 
   // Event listener for reset button and clicker
   clickButton.addEventListener("click", onClick);
