e376212b91613445e0fd95f429d0b5ed5fff56a5
Mika <pizzafan513@gmail.com>
2023-12-02 12:38:01 -0500

    add notes

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+
+// window.onLoad so script does not run before DOM fully loads
 window.onload = function () {
+    
+// declare count variable
   let count = 0;
+
+  // declare button from DOM
   let btn = document.getElementById("btn");
+
+  // declare count display from dom, where count will be displayed
   let disp = document.getElementById("display");
 
   btn.addEventListener("click", function () {
